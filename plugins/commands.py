21924124341276449fc394d4075fa2a03cf6c4e1
Royalbots6 <111477575+Royalbots6@users.noreply.github.com>
2022-08-26 18:13:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -43,12 +43,12 @@ async def start(client, message):
     if len(message.command) != 2:
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
-            ],[
+        ], [
+            InlineKeyboardButton('📢 Channel ', url='https://t.me/wombackup'),
+            InlineKeyboardButton('😎 Creator', url='https://t.me/Royaldeep01')
+        ], [
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('😋 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
